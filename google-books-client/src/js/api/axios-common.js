9522a5d3faa372dc008911e71496b37a23b0979a
eduardosmatheus <matheus-edu@outlook.com>
2021-01-31 16:45:44 -0300

    Adjust axios config param.

--- google-books-client/src/js/api/axios-common.js
+++ google-books-client/src/js/api/axios-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-  url: 'https://www.googleapis.com/books/v1',
+  baseURL: 'https://www.googleapis.com/books/v1',
   params: {
     key: process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
   } 
