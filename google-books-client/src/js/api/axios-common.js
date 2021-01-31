523183866af1930af77a234700e0a33c844c9e6f
eduardosmatheus <matheus-edu@outlook.com>
2021-01-31 11:44:24 -0300

    Setup api defaults

---
+++ google-books-client/src/js/api/axios-common.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const apiInstance = axios.create({
+  url: 'https://www.googleapis.com/books/v1',
+  params: {
+    key: process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
+  } 
+});
+
+export default apiInstance;
