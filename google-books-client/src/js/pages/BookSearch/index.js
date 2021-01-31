36cfb68157cb3dbdf6707fa6d906c17fe7e04e2e
eduardosmatheus <matheus-edu@outlook.com>
2021-01-31 16:46:10 -0300

    (WIP) Implement book list.

--- google-books-client/src/js/pages/BookSearch/index.js
+++ google-books-client/src/js/pages/BookSearch/index.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react';
 import api from '../../api';
-import Styles from 'index.module.css';
+import Styles from './index.module.css';
+
+function Book({
+  volumeInfo,
+}) {
+  return (
+    <div className={Styles.Book}>
+      Um livro
+    </div>
+  )
+}
+
+function BookList({ books }) {
+  if (!books) return null;
+  return (
+    <div className={Styles.BookList}>
+      {books.map(Book)}
+    </div>
+  )
+}
 
 export default function BookSearch() {
   const [searchHint, setSearchHint] = useState('');
@@ -9,7 +28,7 @@ export default function BookSearch() {
   const handleSearch = async (e) => {
     e.preventDefault();
     const { data } = await api.books.search(searchHint);
-    setBooks(data);
+    setBooks(data.items);
   };
 
   return (
@@ -23,7 +42,7 @@ export default function BookSearch() {
         />
         <input type="submit" name="Pesquisar" />
       </form>
-
+      <BookList books={books} />
     </div>
   )
 }
