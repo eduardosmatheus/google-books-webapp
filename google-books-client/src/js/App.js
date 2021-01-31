7c7c29ae95efab4d5e5b95d0c88c151af4f4f31c
eduardosmatheus <matheus-edu@outlook.com>
2021-01-31 16:45:33 -0300

    Implement main app's routing

--- google-books-client/src/js/App.js
+++ google-books-client/src/js/App.js
@@ -1,21 +1,25 @@
+import { Link, Route, Switch } from 'react-router-dom';
 import '../css/App.css';
+import BookSearch from './pages/BookSearch';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/books">
+          Buscar Livros
+        </Link>
+        <Link to="/favorites">
+          Meus Favoritos
+        </Link>
       </header>
+      <main>
+        <Switch>
+          <Route path="/books" exact component={BookSearch} />
+          <Route path="/favorites" exact component={Favorites} />
+        </Switch>
+      </main>
     </div>
   );
 }
